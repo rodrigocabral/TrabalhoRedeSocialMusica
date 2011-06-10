0d0681ec209a9a12f027ccd561871962307beafa
Robson de Jesus <robson.djsilva@gmail.com>
2011-06-10 09:51:18 -0300

    script do banco com tabelas amigos, solicitações e recados
    Signed-off-by: Robson de Jesus <robson.djsilva@gmail.com>

--- banco_de_dados/openmusic.sql
+++ banco_de_dados/openmusic.sql
@@ -17,10 +17,76 @@ CREATE  TABLE IF NOT EXISTS `openmusic`.`usuarios` (
   `sexo` VARCHAR(1) NOT NULL ,
   `datanascimento` DATE NOT NULL ,
   `foto` VARCHAR(200) NULL ,
+  `cidade` VARCHAR(100) NOT NULL ,
+  `estado` VARCHAR(2) NOT NULL ,
+  `pais` VARCHAR(100) NOT NULL ,
   PRIMARY KEY (`id`) )
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = latin1
 COLLATE = latin1_swedish_ci;
+
+
+-- -----------------------------------------------------
+-- Table `openmusic`.`solicitacoes`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `openmusic`.`solicitacoes` (
+  `id` INT NOT NULL AUTO_INCREMENT ,
+  `idsolicitador` INT NOT NULL ,
+  `idsolicitado` INT NOT NULL ,
+  PRIMARY KEY (`id`) ,
+  INDEX `idsolicitador` (`id` ASC) ,
+  INDEX `idsolicitado` (`id` ASC) ,
+  CONSTRAINT `idsolicitador`
+    FOREIGN KEY (`id` )
+    REFERENCES `openmusic`.`usuarios` (`id` )
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  CONSTRAINT `idsolicitado`
+    FOREIGN KEY (`id` )
+    REFERENCES `openmusic`.`usuarios` (`id` )
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `openmusic`.`amigos`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `openmusic`.`amigos` (
+  `id` INT NOT NULL ,
+  `idamigo1` INT NOT NULL ,
+  `idamigo2` INT NOT NULL ,
+  PRIMARY KEY (`id`) ,
+  INDEX `idamigo1` (`id` ASC) ,
+  INDEX `idamigo2` (`id` ASC) ,
+  CONSTRAINT `idamigo1`
+    FOREIGN KEY (`id` )
+    REFERENCES `openmusic`.`usuarios` (`id` )
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  CONSTRAINT `idamigo2`
+    FOREIGN KEY (`id` )
+    REFERENCES `openmusic`.`usuarios` (`id` )
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `openmusic`.`recado`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `openmusic`.`recado` (
+  `id` INT NOT NULL ,
+  `idamigos` INT NOT NULL ,
+  `recado` LONGTEXT NOT NULL ,
+  PRIMARY KEY (`id`) ,
+  INDEX `idamigos` (`id` ASC) ,
+  CONSTRAINT `idamigos`
+    FOREIGN KEY (`id` )
+    REFERENCES `openmusic`.`amigos` (`id` )
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
+ENGINE = InnoDB;
 
 
 

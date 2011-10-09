6700ed1e994fc4f9f64f5ed7a316af415a671e1c
Robson de Jesus <robson.djsilva@gmail.com>
2011-10-09 18:07:59 -0300

    arquivo workbench
    Signed-off-by: Robson de Jesus <robson.djsilva@gmail.com>

--- banco_de_dados/openmusic.sql
+++ banco_de_dados/openmusic.sql
@@ -1,107 +1,144 @@
--- phpMyAdmin SQL Dump
--- version 2.10.1
--- http://www.phpmyadmin.net
--- 
--- Servidor: localhost
--- Tempo de Geração: Ago 24, 2011 as 10:36 PM
--- Versão do Servidor: 5.0.45
--- Versão do PHP: 5.2.5
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL';
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+CREATE SCHEMA IF NOT EXISTS `openmusic` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci ;
+USE `openmusic` ;
 
--- 
--- Banco de Dados: 'openmusic'
--- 
+-- -----------------------------------------------------
+-- Table `openmusic`.`usuarios`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `openmusic`.`usuarios` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT ,
+  `nome` VARCHAR(45) NOT NULL ,
+  `sobrenome` VARCHAR(100) NULL DEFAULT NULL ,
+  `email` VARCHAR(100) NOT NULL ,
+  `senha` VARCHAR(45) NOT NULL ,
+  `sexo` VARCHAR(1) NOT NULL ,
+  `datanascimento` DATE NOT NULL ,
+  `foto` VARCHAR(200) NULL ,
+  `cidade` VARCHAR(100) NOT NULL ,
+  `estado` VARCHAR(2) NOT NULL ,
+  `pais` VARCHAR(100) NOT NULL ,
+  PRIMARY KEY (`id`) )
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = latin1
+COLLATE = latin1_swedish_ci;
 
--- --------------------------------------------------------
 
--- 
--- Estrutura da tabela 'amigos'
--- 
+-- -----------------------------------------------------
+-- Table `openmusic`.`solicitacoes`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `openmusic`.`solicitacoes` (
+  `id` INT NOT NULL AUTO_INCREMENT ,
+  `idsolicitador` INT NOT NULL ,
+  `idsolicitado` INT NOT NULL ,
+  PRIMARY KEY (`id`) ,
+  INDEX `idsolicitador` (`id` ASC) ,
+  INDEX `idsolicitado` (`id` ASC) ,
+  CONSTRAINT `idsolicitador`
+    FOREIGN KEY (`id` )
+    REFERENCES `openmusic`.`usuarios` (`id` )
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  CONSTRAINT `idsolicitado`
+    FOREIGN KEY (`id` )
+    REFERENCES `openmusic`.`usuarios` (`id` )
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
+ENGINE = InnoDB;
 
-CREATE TABLE amigos (
-  id int(11) NOT NULL auto_increment,
-  idamigo1 int(11) NOT NULL,
-  idamigo2 int(11) NOT NULL,
-  PRIMARY KEY  (id),
-  KEY idamigo1 (id),
-  KEY idamigo2 (id)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
+-- -----------------------------------------------------
+-- Table `openmusic`.`amigos`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `openmusic`.`amigos` (
+  `id` INT NOT NULL ,
+  `idamigo1` INT NOT NULL ,
+  `idamigo2` INT NOT NULL ,
+  PRIMARY KEY (`id`) ,
+  INDEX `idamigo1` (`id` ASC) ,
+  INDEX `idamigo2` (`id` ASC) ,
+  CONSTRAINT `idamigo1`
+    FOREIGN KEY (`id` )
+    REFERENCES `openmusic`.`usuarios` (`id` )
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  CONSTRAINT `idamigo2`
+    FOREIGN KEY (`id` )
+    REFERENCES `openmusic`.`usuarios` (`id` )
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
+ENGINE = InnoDB;
 
--- 
--- Estrutura da tabela 'recado'
--- 
 
-CREATE TABLE recado (
-  id int(11) NOT NULL,
-  idusuario int(11) NOT NULL,
-  idamigo int(11) NOT NULL,
-  recado longtext NOT NULL,
-  PRIMARY KEY  (id),
-  KEY idamigos (id),
-  KEY idamigo (idamigo),
-  KEY idusuario (idusuario)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+-- -----------------------------------------------------
+-- Table `openmusic`.`recado`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `openmusic`.`recado` (
+  `id` INT NOT NULL AUTO_INCREMENT ,
+  `recado` LONGTEXT NOT NULL ,
+  `idusuario` INT NOT NULL ,
+  `idamigo` INT NOT NULL ,
+  PRIMARY KEY (`id`) ,
+  INDEX `idenviou` (`id` ASC) ,
+  INDEX `idrecebeu` (`id` ASC) ,
+  CONSTRAINT `idenviou`
+    FOREIGN KEY (`id` )
+    REFERENCES `openmusic`.`usuarios` (`id` )
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  CONSTRAINT `idrecebeu`
+    FOREIGN KEY (`id` )
+    REFERENCES `openmusic`.`usuarios` (`id` )
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
+ENGINE = InnoDB;
 
--- --------------------------------------------------------
 
--- 
--- Estrutura da tabela 'solicitacoes'
--- 
+-- -----------------------------------------------------
+-- Table `openmusic`.`musicas`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `openmusic`.`musicas` (
+  `id` INT NOT NULL AUTO_INCREMENT ,
+  `idusuario` INT NOT NULL ,
+  `nome` VARCHAR(300) NULL ,
+  `arquivo` VARCHAR(300) NULL ,
+  PRIMARY KEY (`id`) ,
+  INDEX `idusuario` (`id` ASC) ,
+  CONSTRAINT `idusuario`
+    FOREIGN KEY (`id` )
+    REFERENCES `openmusic`.`usuarios` (`id` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
 
-CREATE TABLE solicitacoes (
-  id int(11) NOT NULL auto_increment,
-  idsolicitador int(11) NOT NULL,
-  idsolicitado int(11) NOT NULL,
-  PRIMARY KEY  (id),
-  KEY idsolicitador (id),
-  KEY idsolicitado (id)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
+-- -----------------------------------------------------
+-- Table `openmusic`.`mixagens`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `openmusic`.`mixagens` (
+  `id` INT NOT NULL AUTO_INCREMENT ,
+  `idusuario` INT NULL ,
+  `idmusica` INT NULL ,
+  `arquivo` VARCHAR(300) NULL ,
+  PRIMARY KEY (`id`) ,
+  INDEX `idusuario` (`id` ASC) ,
+  INDEX `idmusica` (`id` ASC) ,
+  CONSTRAINT `idusuario`
+    FOREIGN KEY (`id` )
+    REFERENCES `openmusic`.`usuarios` (`id` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `idmusica`
+    FOREIGN KEY (`id` )
+    REFERENCES `openmusic`.`musicas` (`id` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
 
--- 
--- Estrutura da tabela 'usuarios'
--- 
 
-CREATE TABLE usuarios (
-  id int(11) NOT NULL auto_increment,
-  nome varchar(45) NOT NULL,
-  sobrenome varchar(100) default NULL,
-  email varchar(100) default NULL,
-  senha varchar(45) default NULL,
-  sexo varchar(15) default NULL,
-  datanascimento date default NULL,
-  foto varchar(200) default NULL,
-  cidade varchar(100) default NULL,
-  estado varchar(2) default NULL,
-  pais varchar(100) default NULL,
-  PRIMARY KEY  (id)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1;
 
--- 
--- Restrições para as tabelas dumpadas
--- 
-
--- 
--- Restrições para a tabela `amigos`
--- 
-ALTER TABLE `amigos`
-  ADD CONSTRAINT idamigo1 FOREIGN KEY (id) REFERENCES usuarios (id) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT idamigo2 FOREIGN KEY (id) REFERENCES usuarios (id) ON DELETE CASCADE ON UPDATE CASCADE;
-
--- 
--- Restrições para a tabela `recado`
--- 
-ALTER TABLE `recado`
-  ADD CONSTRAINT idamigo FOREIGN KEY (idamigo) REFERENCES usuarios (id) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT idusuario FOREIGN KEY (idusuario) REFERENCES usuarios (id) ON DELETE CASCADE ON UPDATE CASCADE;
-
--- 
--- Restrições para a tabela `solicitacoes`
--- 
-ALTER TABLE `solicitacoes`
-  ADD CONSTRAINT idsolicitado FOREIGN KEY (id) REFERENCES usuarios (id) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT idsolicitador FOREIGN KEY (id) REFERENCES usuarios (id) ON DELETE CASCADE ON UPDATE CASCADE;
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
